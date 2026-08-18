5e48c86096d0c2fb4a36d43c9881eb996baf8542
Binay432 <sahbinay432@gmail.com>
2026-08-18 15:46:22 +0200

    apply .gitiginore, stop tracking build artifacts

--- Debug/Src/004button_interrupt.d
+++
@@ -1,5 +0,0 @@
-Src/004button_interrupt.o: ../Src/004button_interrupt.c \
- D:/Learning/Embedded\ system/STM32_Core/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h \
- D:/Learning/Embedded\ system/STM32_Core/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/Src/stm32f407xx_gpio_driver.h
-D:/Learning/Embedded\ system/STM32_Core/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h:
-D:/Learning/Embedded\ system/STM32_Core/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/Src/stm32f407xx_gpio_driver.h:
